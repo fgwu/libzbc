5b35e68f0ada2c64720cdd8ee069d5cb2bc97764
Ming-Chang Yang <riddle216@gmail.com>
2015-12-07 23:43:17 -0600

    Modify to fit with the new script format.

--- script/random_script_gen.sh
+++ script/random_script_gen.sh
@@ -4,8 +4,8 @@ for i in `seq 1 $1`
 do
 #    echo $RANDOM
 	if [ $2 -ge 64 ] && [ $2 -le 29809 ]; then
-		echo    `shuf -i 64-$2 -n 1` `shuf -i 0-524288 -n 1` 8 4096 1
+		echo    `shuf -i 64-$2 -n 1` `shuf -i 0-524288 -n 1` 8 1
 	else
-		echo    `shuf -i 64-29809 -n 1` `shuf -i 0-524288 -n 1` 8 4096 1
+		echo    `shuf -i 64-29809 -n 1` `shuf -i 0-524288 -n 1` 8 1
 	fi
 done
